c298da3cc602bc243ab39f7980d8acd37b6def3e
Marek Snyder <mr.snyder31@gmail.com>
2023-03-14 10:12:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pterrordactyls_heist_part_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
